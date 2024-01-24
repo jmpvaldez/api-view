c2e837ca5aa9a9ad70525f31f4bafb40215ac49d
jpvaldez <jpvaldez@dswd.gov.ph>
2024-01-24 11:34:50 +0800

    da

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import Script from 'next/script'
+
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Script from 'next/script'
+
 
 export default function Home() {
   return (
    <main>
-    
+
      <Script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js"></Script>
    </main>
   );
